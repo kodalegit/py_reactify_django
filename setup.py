b413025cc7833e8d4e53c7e0e3c34c4ee781fea1
kodalegit <victorkimani77@gmail.com>
2024-09-03 00:39:54 +0300

    add setup file and tests dir

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="reactify-django",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "Django>=3.0",
+        "click",
+        # Add any other dependencies here
+    ],
+    entry_points={
+        "console_scripts": [
+            "reactify-django=src.cli:setup_django_react",
+        ],
+    },
+    include_package_data=True,
+    description="CLI tool to configure React and TypeScript in Django",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kodalegit/react-in-django",
+    author="Victor Kimani",
+    author_email="victorkimani77@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
