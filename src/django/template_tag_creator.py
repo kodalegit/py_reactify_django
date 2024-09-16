7efae719892ec7eebeef0abd4f4f71e33e44aa1e
kodalegit <victorkimani77@gmail.com>
2024-09-17 00:15:17 +0300

    modify template tag creator

--- src/django/template_tag_creator.py
+++ src/django/template_tag_creator.py
@@ -1,16 +1,34 @@
 import os
 
 
-def create_template_tag():
-    tag_code = """
-    from django import template
-    register = template.Library()
+def create_template_tag(app_name):
+    tag_code = f"""
+from django import template
+from django.templatetags.static import static
 
-    @register.simple_tag
-    def react_root():
-        return '<div id="root"></div>'
-    """
-    tag_path = "your_app/templatetags/react_tags.py"
+register = template.Library()
+
+@register.simple_tag
+def react_root():
+    # Create the URL for the React bundled script using Django's static tag
+    script_url = static('{app_name}/js/bundle.js')
+    return f'''
+<div id="root"></div>
+<script src="{{{{ script_url }}}}"></script>
+'''
+"""
+
+    tag_path = f"{app_name}/templatetags/react_root.py"
     os.makedirs(os.path.dirname(tag_path), exist_ok=True)
     with open(tag_path, "w") as tag_file:
         tag_file.write(tag_code)
+
+    init_path = os.path.join(app_name, "templatetags", "__init__.py")
+    with open(init_path, "w") as init_file:
+        init_file.write(
+            "# This file makes the templatetags directory a Python package\n"
+        )
+
+    print(
+        f"Custom template tag and __init__.py file created in {app_name}/templatetags/"
+    )
